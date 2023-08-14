ccbeb24232dcc521b35046a3ab352fa9976cebc2
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-08-14 12:16:57 -0700

    basic login logic

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -9,22 +9,19 @@ class Api::SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    @user = User.find_by_credentials(params[:credential], params[:password])
 
     if @user
       login!(@user)
       render 'api/users/show'
     else
-      render json: ['Invalid email or password'], status: 401
+      render json: { errors: ['The provided credentials were invalid.'] }, 
+        status: :unauthorized
     end
   end
 
   def destroy
-    if current_user
-      logout!
-      render json: {message: 'Logout successful'}
-    else
-      render json: ['No user to logout'], status: 404
-    end
+    logout!
+    render json: { message: 'success' }
   end
 end
